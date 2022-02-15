54706c01e284c2a312ac357c2386dcb93ec58d69
lastchancebar <darinaduffykelly@gmail.com>
2022-02-15 20:00:16 +0000

    add function names to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame(){}
+
+function checkAnswer(){}
+
+function calculateCorrectAnswer(){}
+
+function incrementScore(){}
+
+function incrementWrongAnswer(){}
+
+function displayAdditionQuestions(){}
+
+function displaySubtractQuestions(){}
+
+function displayMultiplyQuestions(){}
