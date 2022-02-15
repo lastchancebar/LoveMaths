22ed0adc3a597c012019c983c1934c0aef95fc17
lastchancebar <darinaduffykelly@gmail.com>
2022-02-15 20:32:02 +0000

    adding functions to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener('click', function() {
+            if (this.getAttribute("data-type")  === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                    let gameType = this.getAttribute("data-type"); 
+                    alert(`You clicked ${gameType}`);
+
+            }
+        }
+        
+        
+        
+        )
+
+    }
+})
+
 function runGame(){}
 
 function checkAnswer(){}
@@ -8,8 +28,8 @@ function incrementScore(){}
 
 function incrementWrongAnswer(){}
 
-function displayAdditionQuestions(){}
+function displayAdditionQuestion(){}
 
-function displaySubtractQuestions(){}
+function displaySubtractQuestion(){}
 
-function displayMultiplyQuestions(){}
+function displayMultiplyQuestion(){}
