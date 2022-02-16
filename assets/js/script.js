0151fcfb8f497ebedc35d7c68357f630576d9e33
lastchancebar <darinaduffykelly@gmail.com>
2022-02-16 12:45:39 +0000

    tidy ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,26 @@ document.addEventListener("DOMContentLoaded", function () {
                     let gameType = this.getAttribute("data-type");
                     runGame(gameType);
                 }
-            }
-        )
+            
+            })
+            
     }
-    runGame("addition");
+    document.getElementById("answer-box").addEventListener("keydown", function(event)
+{ if (event.key ==="Enter"){
+    checkAnswer();
+}
+
 })
+        runGame("addition");
+  })
+  
 /** 
  * The game "loop", called when the script is loaded 
  * and after the user's answer has been processed */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     //creates 2 random numbers between 1 and 25;
     let num1 = Math.floor(Math.random() * 25) + 1;
