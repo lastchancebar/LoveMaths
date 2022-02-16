4b5c55e91a2cdf65e84cc7087f676e4d6c2145e4
lastchancebar <darinaduffykelly@gmail.com>
2022-02-16 11:05:33 +0000

    add subtract game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,10 @@ if (gameType === "addition") {
 
 }else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+
+}else if (gameType === "subtraction") {
+        displaySubtractQuestion(num1, num2);
+
 }else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -62,6 +66,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x"){
             return [operand1 * operand2, "multiply"];
+        } else if (operator === "-"){
+            return [operand1  - operand2, "subtraction"];
     }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -91,7 +97,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand2 < operand1 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+}
 
 
 function displayMultiplyQuestion(operand1,operand2) {
